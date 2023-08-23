1b9271802bcfbf6d27c67335e0b763e4e91d4fd0
Jibbril Berbres <jibbril.b.n@gmail.com>
2023-08-23 14:04:09 +0200

    Setup dialog through dependents

--- app/dependents/webapp/controller/View1.controller.js
+++ app/dependents/webapp/controller/View1.controller.js
@@ -1,15 +1,44 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/Dialog",
+    "sap/m/Text",
+    "sap/m/Button",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/VBox",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Dialog, Text, Button, JSONModel, VBox) {
         "use strict";
 
         return Controller.extend("ns.dependents.controller.View1", {
             onInit: function () {
+                const model = this.getOwnerComponent().getModel();
+                this.getView().setModel(model);
+            },
 
+            onButtonPressed: function (event) {
+                const dialog = new Dialog({ 
+                    title: "Hello World",
+                    content: new VBox({
+                        items: [
+                            new Text({ text: "{name}" }),
+                            new Text({ text: "{dialog>/name}" }),
+                        ]
+                    }),
+                    buttons: [  
+                        new Button({ text: "Close", press: () => dialog.close() })
+                    ]
+                });
+
+                const button = event.getSource();
+                const model = new JSONModel({ name: "From dialog model attached to button" });
+                button.setModel(model, "dialog");
+
+                button.addDependent(dialog);
+
+                dialog.open();
             }
         });
     });
